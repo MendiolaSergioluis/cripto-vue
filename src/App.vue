883567ec67727721b729119920d5602cc612e087
Sergioluis Mendiola <mendiolasergioluis@gmail.com>
2024-04-22 16:19:51 -0400

    1. Agregada validación del formulario 2. Se generó un alerta tipo error, para advertir cuando el formulario está incompleto

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import {ref, reactive, onMounted} from 'vue'
+import AlertaError from "./components/AlertaError.vue";
 
 // Genera la variable reactiva con las monedas disponibles en la aplicación
 const monedas = ref([
@@ -14,11 +15,13 @@ const monedas = ref([
 
 // Genera la variable reactiva para contener las principales criptomonedas
 const criptomonedas = ref([])
-// Cotización recibida del formulario
+// Se genera un objeto de cotización con la información recibida del formulario
 const cotizar = reactive({
   moneda: '',
   criptomoneda: ''
 })
+// Constante que almacena los mensajes de error
+const error = ref('')
 
 // Agrega los datos de la API a la aplicación cuando esta se inicia
 onMounted(async () => {
@@ -27,13 +30,28 @@ onMounted(async () => {
   criptomonedas.value = (await respuesta.json()).Data // Agrega las principales criptomonedas desde la API
 })
 
+const cotizarCripto = () => {
+  // Validar que el objeto cotizar este lleno
+  if (Object.values(cotizar).includes('')) {
+    error.value = 'Todos los campos son obligatorios' // Agrega mensaje de error
+    setTimeout(() => {
+      error.value = '' // Vacía el contenido de la variable 'error'
+    }, 3000) // El mensaje de alerta se borra después de 3 segundos
+    return null
+  }
+}
+
 </script>
 
 <template>
   <div class="contenedor">
     <h1 class="titulo">Cotizador de <span>Criptomonedas</span></h1>
     <div class="contenido">
-      <form class="formulario">
+      <AlertaError v-if="error">{{ error }}</AlertaError>
+      <form
+          class="formulario"
+          @submit.prevent="cotizarCripto"
+      >
         <!-- Selector de Moneda -->
         <div class="campo">
           <label for="moneda">Moneda:</label>
