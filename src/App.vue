5080830ae285fe44563b245f376f2a76d39cebde
Sergioluis Mendiola <mendiolasergioluis@gmail.com>
2024-04-22 11:56:55 -0400

    1. Inicio de formulario 2. Agregado selector de moneda.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,38 @@
 <script setup>
+import {ref} from 'vue'
+
+const monedas = ref([
+  {codigo: 'USD', texto: 'Dolar de Estados Unidos'},
+  {codigo: 'EUR', texto: 'Euro'},
+  {codigo: 'CLP', texto: 'Peso Chileno'},
+  {codigo: 'ARS', texto: 'Peso Argentino'},
+  {codigo: 'PEN', texto: 'Sol Peruano'},
+  {codigo: 'MXN', texto: 'Peso Mexicano'},
+  {codigo: 'GBP', texto: 'Libra Esterlina'},
+])
 
 </script>
 
 <template>
-
+  <div class="contenedor">
+    <h1 class="titulo">Cotizador de <span>Criptomonedas</span></h1>
+    <div class="contenido">
+      <form class="formulario">
+        <div class="campo">
+          <label for="moneda">Moneda:</label>
+          <select id="moneda">
+            <option value="">-- Selecciona --</option>
+            <option
+                v-for="moneda in monedas"
+                :value="moneda.codigo"
+            >
+              {{ moneda.texto }}
+            </option>
+          </select>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
 <style scoped>
